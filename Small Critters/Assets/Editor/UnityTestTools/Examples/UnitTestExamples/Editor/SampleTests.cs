8d27337a8d6e529ddf97f37acc043c632f38e52d
Greg-Rus <grzegorz.rus@gmail.com>
2015-12-02 22:02:41 +0100

    Project Prep for Refactor
    Made new scene for refactoring, new folder structure. Started unity test
    file.

--- Small Critters/Assets/Editor/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
+++ Small Critters/Assets/Editor/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
@@ -1,3 +1,4 @@
+/*
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -100,3 +101,4 @@ namespace UnityTest
         }
     }
 }
+*/
---
+++ Small Critters/Assets/Editor/UnityTestTools/Examples/UnitTestExamples/Editor/SmallCrittersGeneralTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UnityTest
+{
+	[TestFixture]
+	[Category("Sample Tests")]
+	internal class BasicTests
+	{
+		[Test]
+		public void sampleAssertion()
+		{
+			Assert.True(true);
+		}
+	}
+}
