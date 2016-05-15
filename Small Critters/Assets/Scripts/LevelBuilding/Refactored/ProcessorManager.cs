d66929275dba4786e219597011a9baad6b15ce13
Greg-Rus <grzegorz.rus@gmail.com>
2016-05-15 21:15:12 +0200

    Performance tweaks to reduce draw calls

--- Small Critters/Assets/Scripts/LevelBuilding/Refactored/ProcessorManager.cs
+++ Small Critters/Assets/Scripts/LevelBuilding/Refactored/ProcessorManager.cs
@@ -3,17 +3,20 @@ using System.Collections;
 
 public class ProcessorManager : MonoBehaviour {
 	SpriteRenderer mySpriteRenderer;
+    Animator myAnimator;
 	public ProcessorState state;
 	public float stateExitTime; //Time.timeSinceLevelLoad + some state timer
 	public float stateStayTimeCompletion; // 0.0-1.0 %
-	private ParticleSystem steamRing;
+	//private ParticleSystem steamRing;
 	
 	// Use this for initialization
 	void Awake () {
 		mySpriteRenderer = GetComponent<SpriteRenderer>();
 		gameObject.layer = 8;
-		steamRing = GetComponent<ParticleSystem>();
-	}
+		//steamRing = GetComponent<ParticleSystem>();
+        myAnimator = GetComponent<Animator>();
+
+    }
 	
 	public void TintProcessorSprite(Color startColor, Color targetColor, float percent)
 	{
@@ -26,14 +29,16 @@ public class ProcessorManager : MonoBehaviour {
 	public void SetHazadrousLayer()
 	{
 		gameObject.layer = 15;
-        steamRing.Simulate(0f, false, true);
-		steamRing.Play();
+        //steamRing.Simulate(0f, false, true);
+        //steamRing.Play();
+        myAnimator.SetTrigger("SteamStart");
     }
 	public void SetSafeLayer()
 	{
-        steamRing.Pause();
-        steamRing.Clear();
-        steamRing.Stop();
+        //steamRing.Pause();
+        //steamRing.Clear();
+        //steamRing.Stop();
+        myAnimator.SetTrigger("SteamEnd");
         gameObject.layer = 8;
         
     }
