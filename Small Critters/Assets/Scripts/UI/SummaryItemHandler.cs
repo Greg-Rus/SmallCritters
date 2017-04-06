2ed0efc3230504e0b029eaadbd8147cf08712049
Greg-Rus <grzegorz.rus@gmail.com>
2017-04-06 17:42:18 +0200

    Cleanup for test build

---
+++ Small Critters/Assets/Scripts/UI/SummaryItemHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SummaryItemHandler : MonoBehaviour {
+    public Text myText;
+    public Text myCount;
+    public Text myPoints;
+    public RectTransform myRect;
+	// Use this for initialization
+
+    public void SetText(string text)
+    {
+        myText.text = text;
+    }
+    public void SetCount(float count)
+    {
+        myCount.text = count.ToString();
+    }
+    public void SetPoints(float points)
+    {
+        myPoints.text = points.ToString();
+    }
+    public void SetPositionY(float y)
+    {
+        Vector3 newPosition = myRect.localPosition;
+        newPosition.y = y;
+        myRect.localPosition = newPosition;
+    }
+}
---
+++ Small Critters/Assets/Scripts/UI/SummaryMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummaryMenuController : MonoBehaviour {
+    public GameObject SummaryItemPrefab;
+    public RectTransform ContentRect;
+    private float summaryItemHeight;
+    private float lastSummaryItemPositionY = 0;
+	// Use this for initialization
+	void Start () {
+        summaryItemHeight = SummaryItemPrefab.GetComponent<RectTransform>().rect.height;
+        Debug.Log(lastSummaryItemPositionY);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DisplaySummaryItem(string text, int count, int points, bool animate =true)
+    {
+        ContentRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, summaryItemHeight * 2f);
+        GameObject newSummaryItem = Instantiate(SummaryItemPrefab, ContentRect) as GameObject;
+        newSummaryItem.transform.SetParent(ContentRect);
+        lastSummaryItemPositionY -= summaryItemHeight;
+        
+        SummaryItemHandler handler = newSummaryItem.GetComponent<SummaryItemHandler>();
+        handler.SetText(text);
+        handler.SetCount(count);
+        handler.SetPoints(points);
+        handler.SetPositionY(lastSummaryItemPositionY);
+    }
+}
